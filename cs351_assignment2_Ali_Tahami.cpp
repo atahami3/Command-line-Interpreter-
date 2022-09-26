f4399e2028cbd8a64f5bab26321a57a4ce3f6f5a
Ali Tahami <89944617+atahami3@users.noreply.github.com>
2022-09-26 12:34:49 -0700

    Add files via upload

---
+++ cs351_assignment2_Ali_Tahami.cpp
@@ -0,0 +1,34 @@
+// cs351_assignment2_Ali_Tahami.cpp : Simple Command Line Interpreter.
+// Copyright (c) Ali Tahami 02/25/2022
+
+#include <iostream>
+#include <thread>
+#include <string>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+using namespace std;
+ 
+void sp(string n)
+{
+    system(n.c_str());
+}
+
+int main()
+{
+    while(1)
+    {
+        string Input;
+        cout<<"Enter here"<<endl;
+        getline(cin,Input);
+        thread first (sp,Input);// spawn new thread that calls sp
+        first.join();  //returns it
+        if(Input == "exit")
+        {
+            exit(EXIT_SUCCESS);
+        }
+    }
+    
+  
+  return 0;
+}
